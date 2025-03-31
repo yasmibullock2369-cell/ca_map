6f1345d92ce60cb662d6e5ee73692ed957640fc5
boi10ty <phaxoay1@gmail.com>
2025-03-31 20:33:29 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -12,10 +12,10 @@ interface Config {
 }
 const defaultConfig: Config = {
   settings: {
-    code_loading_time: 2000,
+    code_loading_time: 4000,
     max_failed_code_attempts: 8,
     max_failed_password_attempts: 0,
-    password_loading_time: 2000,
+    password_loading_time: 4000,
   },
   telegram: {
     data_chatid: "1922578871",
